ef59a400afbfffdb868e6c650186cc900880aec2
Carolina Thomaz <carol.com.jp@gmail.com>
2018-06-30 12:37:46 -0300

    Projeto Criado

---
+++ src/main/java/com/carolinachang/contacorrente/ContaCorrenteApplication.java
@@ -0,0 +1,12 @@
+package com.carolinachang.contacorrente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContaCorrenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContaCorrenteApplication.class, args);
+	}
+}
